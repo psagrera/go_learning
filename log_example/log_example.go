b5f1c7ed8e317f94b942fb10e330269b724b8226
pablo <psagrera@juniper.net>
2017-04-10 16:26:14 +0200

    adding more examples

---
+++ log_example/log_example.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+	"log"
+	"os"
+)
+
+type Job struct {
+	Command string
+	*log.Logger
+}
+
+func main() {
+	job := &Job{"demo",log.New(os.Stderr, "Job: ", log.Ldate)}
+	job.Print("Starting now")
+}
+
